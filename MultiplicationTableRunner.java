4aa829f7e6fa303f096fd7baa0e59454d5d8977a
unknown <shivamaware@gmail.com>
2020-05-26 23:42:04 +0530

    Multiplication table

---
+++ MultiplicationTableRunner.java
@@ -0,0 +1,13 @@
+package com.Shivam.FirstJavaPoject;
+
+public class MultiplicaionTableRunner {
+
+	public static void main(String[] args) {
+		MultiplicationTable table = new MultiplicationTable();
+		table.print();	
+		//table.print(6);
+		//table.print(7,11,20);
+		
+	}
+
+}
